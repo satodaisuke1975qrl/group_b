687b4058b0bcae61ac1d008ea9558f8ce18d1eac
cathy831 <cathy_831@icloud.com>
2023-06-22 11:30:02 +0900

    foms.pyとviews.pyの雛形作成

--- tv_listing/tvasahi/views.py
+++ tv_listing/tvasahi/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
+from .forms import CommentCreateForm, CommentUpdateForm
 
 # Create your views here.
 
 
 class Home(generic.TemplateView):
     template_name = 'tvasahi/home.html'
+
+
+class TvListView(generic.ListView):
+
+
+class TvDetailView(generic.DetailView):
+
+
+class CommentCreateView(generic.CreateView):
+
+
+class CommentListView(generic.ListView):
+
+
+class CommentUpdateView(generic.UpdateView):
+
+
+class CommentDeleteView(generic.DeleteView):
