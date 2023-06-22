24d52e5a0acb981a00bfab2ebae4435d96449ac8
satodaisuke1975qrl <satodaisuke.1975.qrl@gmail.com>
2023-06-22 11:34:38 +0900

    README追記

--- tv_listing/tvasahi/views.py
+++ tv_listing/tvasahi/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
 
+
 # Create your views here.
 
 
 class Home(generic.TemplateView):
     template_name = 'tvasahi/home.html'
+
+
+class CommentCreateView(generic.CreateView):
