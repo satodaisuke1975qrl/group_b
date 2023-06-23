812b92aabb9937c1c44a2dba808bcd57956f8fe8
matsutanihhh <ichigo11421@ezweb.ne.jp>
2023-06-23 11:43:38 +0900

    日にちごとに番組表出せるよ

--- tv_listing/tvasahi/urls.py
+++ tv_listing/tvasahi/urls.py
@@ -5,7 +5,7 @@ app_name = 'tvasahi'
 
 urlpatterns = [
     path('home/', views.Home.as_view(), name='home'),
-    path('tvlist/', views.TvListView.as_view(), name='tv_list'),
+    path('tvlist/<int:pk>/', views.TvListView.as_view(), name='tv_list'),
     path('tvdetail/<int:pk>/', views.TvDetailView.as_view(), name='tv_detail'),
     path('tvsearch/', views.SearchView.as_view(), name='tv_search')
     ]
