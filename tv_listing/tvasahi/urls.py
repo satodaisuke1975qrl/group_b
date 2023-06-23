38dd7e7cfc78ea16e2cccdd51d12e1f2bdf0978f
matsutanihhh <ichigo11421@ezweb.ne.jp>
2023-06-23 10:27:58 +0900

    detail作ったよ

--- tv_listing/tvasahi/urls.py
+++ tv_listing/tvasahi/urls.py
@@ -5,5 +5,6 @@ app_name = 'tvasahi'
 
 urlpatterns = [
     path('home/', views.Home.as_view(), name='home'),
-    path('tvlist/', views.TvListView.as_view(), name='tv_list')
+    path('tvlist/', views.TvListView.as_view(), name='tv_list'),
+    path('tvdetail/<int:pk>/', views.TvDetailView.as_view(), name='tv_detail')
     ]
