2db834a1631880169c1448ede24452dafe7dc610
matsutanihhh <ichigo11421@ezweb.ne.jp>
2023-06-23 16:50:57 +0900

    コメント投稿画面ができました

--- tv_listing/tvasahi/urls.py
+++ tv_listing/tvasahi/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('home/', views.Home.as_view(), name='home'),
     path('tvlist/<int:pk>/', views.TvListView.as_view(), name='tv_list'),
     path('tvdetail/<int:pk>/', views.TvDetailView.as_view(), name='tv_detail'),
-    path('tvsearch/', views.SearchView.as_view(), name='tv_search')
+    path('tvsearch/', views.SearchView.as_view(), name='tv_search'),
+    path('comment_create/<int:pk>/', views.CommentCreateView.as_view(), name='comment_create')
     ]
