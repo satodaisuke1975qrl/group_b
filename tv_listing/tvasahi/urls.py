63b2c33eed6a646e08b4cdca2071fcd8dc5d16a4
matsutanihhh <ichigo11421@ezweb.ne.jp>
2023-06-23 09:50:20 +0900

    tvlistのテンプレとurlwo

--- tv_listing/tvasahi/urls.py
+++ tv_listing/tvasahi/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'tvasahi'
 
 urlpatterns = [
-    path('home/', views.Home.as_view(), name='home')
+    path('home/', views.Home.as_view(), name='home'),
+    path('tvlist/', views.TvListView.as_view(), name='tv_list')
     ]
