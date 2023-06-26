528ca56fd40edaf2193112369b249c185edfc15e
matsutanihhh <ichigo11421@ezweb.ne.jp>
2023-06-26 10:53:49 +0900

    ユーザー登録ページを作成しました

--- tv_listing/tvasahi/urls.py
+++ tv_listing/tvasahi/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('tvlist/<int:pk>/', views.TvListView.as_view(), name='tv_list'),
     path('tvdetail/<int:pk>/', views.TvDetailView.as_view(), name='tv_detail'),
     path('tvsearch/', views.SearchView.as_view(), name='tv_search'),
-    path('comment_create/<int:pk>/', views.CommentCreateView.as_view(), name='comment_create')
+    path('comment_create/<int:pk>/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('user_create/', views.CustomUserCreationView.as_view(), name='user_create')
     ]
